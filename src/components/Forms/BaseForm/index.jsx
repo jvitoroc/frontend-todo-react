549cee2e6e7f7f2ac3b8978d21b2574228aae51f
João Vitor de Oliveira Carlos <joao_estilo2012@hotmail.com>
2020-11-08 20:47:09 -0300

    baseform

--- src/components/Forms/BaseForm/index.jsx
+++ src/components/Forms/BaseForm/index.jsx
@@ -4,43 +4,41 @@ import { Formik, Form } from 'formik';
 import Overlay from '../Overlay';
 import ErrorMessage from '../ErrorMessage';
 
-class BaseForm extends Component {
-    render() { 
-        let {after, children, state, initialValues, validate, onSubmit} = {...this.props};
-        let index;
-        
-        switch(state){
-            case 'LOADING': index = 0; break;
-            case 'SUCCESS': index = 1; break;
-            default: index = -1;
-        }
-
-        return (
-            <div className={classes.BaseForm}>
-                <Overlay
-                    className={classes["form-wrapper"]}
-                    index={index}
-                >
-                    <div className={classes["logo"]}/>
-                    <Formik
-                        initialValues={initialValues}
-                        validate={validate}
-                        onSubmit={onSubmit}
-                    >
-                        {(formProps) => {
-                            return (
-                                <Form>
-                                    <ErrorMessage className={classes['error-message']} message={formProps.errors['*']}/>
-                                    {children(formProps)}
-                                </Form>
-                            )
-                        }}
-                    </Formik>
-                    {after ? after:null}
-                </Overlay>
-            </div>
-        );
+function BaseForm(props){
+    let {after, children, state, initialValues, validate, onSubmit} = {...props};
+    let index;
+    
+    switch(state){
+        case 'LOADING': index = 0; break;
+        case 'SUCCESS': index = 1; break;
+        default: index = -1;
     }
+
+    return (
+        <div className={classes.BaseForm}>
+            <Overlay
+                className={classes["form-wrapper"]}
+                index={index}
+            >
+                <div className={classes["logo"]}/>
+                <Formik
+                    initialValues={initialValues}
+                    validate={validate}
+                    onSubmit={onSubmit}
+                >
+                    {(formProps) => {
+                        return (
+                            <Form>
+                                <ErrorMessage className={classes['error-message']} message={formProps.errors['*']}/>
+                                {children(formProps)}
+                            </Form>
+                        )
+                    }}
+                </Formik>
+                {after ? after:null}
+            </Overlay>
+        </div>
+    );
 }
 
 export default BaseForm;
