1651e538acd04991f371d3aebbefb8201959c29f
João Vitor de Oliveira Carlos <joao_estilo2012@hotmail.com>
2020-11-08 20:23:22 -0300

    formoverlay

--- src/components/Forms/Overlay/index.jsx
+++ src/components/Forms/Overlay/index.jsx
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './style.module.css';
 import Overlay from '../../Overlay';
 import ReactLoading from 'react-loading';
 import {IoMdCheckmark} from 'react-icons/io'
 
-class FormOverlay extends Component {
-    render() { 
-        let {children, index, ...otherProps} = {...this.props};
-        
-        return (
-            <Overlay
-                {...otherProps}
-                index={index}
-                states={
-                    [
-                        ()=><div className={classes['overlay']}><ReactLoading type={'spin'} color={'gray'} height={'50px'} width={'50px'}/></div>,
-                        ()=><div className={classes['overlay']}><IoMdCheckmark color={'gray'} size={50}/></div>
-                    ]
-                }
-            >
-                {children}
-            </Overlay>
-        );
-    }
+function FormOverlay(props){
+    let {children, index, ...otherProps} = {...props};
+    
+    return (
+        <Overlay
+            {...otherProps}
+            index={index}
+            states={
+                [
+                    ()=><div className={classes['overlay']}><ReactLoading type={'spin'} color={'gray'} height={'50px'} width={'50px'}/></div>,
+                    ()=><div className={classes['overlay']}><IoMdCheckmark color={'gray'} size={50}/></div>
+                ]
+            }
+        >
+            {children}
+        </Overlay>
+    );
 }
 
 export default FormOverlay;
