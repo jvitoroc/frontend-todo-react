bda603be9d2b4185d01e7ddc6c4914b17000fece
João Vitor de Oliveira Carlos <joao_estilo2012@hotmail.com>
2020-11-08 20:33:53 -0300

    inputtext

--- src/components/Forms/InputText/index.jsx
+++ src/components/Forms/InputText/index.jsx
@@ -1,33 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Field } from 'formik';
 import ErrorMessage from '../ErrorMessage';
 import classes from './style.module.css';
 
-class InputText extends Component {
-    static defaultProps = {
-        password: false
-    };
+function InputText(props = {password: false}){
+    // const lastErrorMessageRef = useRef(null);
 
-    componentWillUpdate({error}){
-        if(error !== undefined)
-            this.lastErrorMessage = error;
-    }
+    // useEffect(()=>{
+    //     if(props.error !== undefined)
+    //         lastErrorMessageRef.current = props.error;
+    // });
 
-    render() { 
-        let {name, label, value, error, password} = {...this.props};
-        let inputClasses = classes['input'] 
-        inputClasses += (value === "" ? ' ' + classes["empty"]:"")
-        inputClasses += (error ? ' ' + classes["error"]:"")
-        
-        return (
-            <div className={classes.InputText}>
-                <Field id={name} className={inputClasses} type={password ? "password":"text"} name={name}/>
-                <label className={classes["label"]} htmlFor={name}>{label}</label>
-                <ErrorMessage message={error}/>
-                <div className={classes['gap']}></div>
-            </div>
-        );
-    }
+    let {name, label, value, error, password} = {...props};
+    let inputClasses = classes['input'] 
+    inputClasses += (value === "" ? ' ' + classes["empty"]:"")
+    inputClasses += (error ? ' ' + classes["error"]:"")
+    
+    return (
+        <div className={classes.InputText}>
+            <Field id={name} className={inputClasses} type={password ? "password":"text"} name={name}/>
+            <label className={classes["label"]} htmlFor={name}>{label}</label>
+            <ErrorMessage message={error}/>
+            <div className={classes['gap']}></div>
+        </div> 
+    );
 }
 
 export default InputText;
